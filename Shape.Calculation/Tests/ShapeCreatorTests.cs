9d5d24f5fad044608c139d1f9a32a13e8facfc44
Damir Alekbaev <alekbaevd@gmail.com>
2024-03-29 10:36:22 +0100

    Tests for SHape Creator have been added

---
+++ Shape.Calculation/Tests/ShapeCreatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Shape.Calculation.Creators;
+using Shape.Calculation.Interfaces;
+using Shape.Calculation.Shapes;
+using Xunit;
+
+namespace Shape.Calculation.Tests
+{
+    public class ShapeCreatorTests
+    {
+        [Fact]
+        public void Create_WithOneDimension_ReturnsCircle()
+        {
+            
+            var dimensions = new double[] { 5 };
+            var shapeCreator = new ShapeCreator(dimensions);
+
+            var shape = shapeCreator.Create();
+
+            Assert.IsType<Circle>(shape);
+        }
+
+        [Fact]
+        public void Create_WithThreeDimensions_ReturnsTriangle()
+        {
+            var dimensions = new double[] { 3, 4, 5 };
+            var shapeCreator = new ShapeCreator(dimensions);
+
+            var shape = shapeCreator.Create();
+
+            Assert.IsType<Triangle>(shape);
+        }
+
+        [Fact]
+        public void Create_WithInvalidNumberOfDimensions_ThrowsArgumentException()
+        {
+            var dimensions = new double[] { }; 
+            var shapeCreator = new ShapeCreator(dimensions);
+
+            Assert.Throws<ArgumentException>(() => shapeCreator.Create());
+        }
+
+        [Fact]
+        public void Create_WithNegativeDimensions_ThrowsIncorrectShapeSizeException()
+        {
+            
+            var dimensions = new double[] { -1 }; // Invalid dimension for a circle
+            var shapeCreator = new ShapeCreator(dimensions);
+
+            Assert.Throws<IncorrectShapeSizeException>(() => shapeCreator.Create());
+        }
+    }
+}
