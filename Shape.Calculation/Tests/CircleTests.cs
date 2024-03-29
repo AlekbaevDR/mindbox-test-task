ac21031980e1cb9d16ae5c208372fb549aa8b062
Damir Alekbaev <alekbaevd@gmail.com>
2024-03-29 02:52:12 +0100

    Tests have been implemented

---
+++ Shape.Calculation/Tests/CircleTests.cs
@@ -0,0 +1,30 @@
+﻿
+using FluentAssertions;
+using Shape.Calculation.Creators;
+using Shape.Calculation.Interfaces;
+using Shape.Calculation.Shapes;
+using Xunit;
+
+namespace Shape.Calculation.Tests
+{
+    public class CircleTests
+    {
+    [Fact]
+    public void Circle_WithValidRadius_CalculatesAreaCorrectly()
+        {
+            var circle = new Circle(5);
+            var expectedArea = Math.PI * Math.Pow(5, 2);
+
+            circle.Area.Should().BeApproximately(expectedArea, 0.01);
+        }
+
+        [Fact]
+        public void Circle_WithZeroRadius_ThrowsIncorrectShapeSizeException()
+        {
+            Action act = () => new CircleCreator(0).Create();
+
+            act.Should().Throw<IncorrectShapeSizeException>()
+                .WithMessage("Circle cannot have such radius 0");
+        }
+    }
+}
---
+++ Shape.Calculation/Tests/TriangleTests.cs
@@ -0,0 +1,42 @@
+﻿
+using FluentAssertions;
+using Shape.Calculation.Creators;
+using Shape.Calculation.Interfaces;
+using Shape.Calculation.Shapes;
+using Xunit;
+
+namespace Shape.Calculation.Tests
+{
+    public class TriangleTests
+    {
+        [Theory]
+        [InlineData(3, 4, 5)]
+        [InlineData(5, 12, 13)]
+        public void Triangle_WithValidSidesAndRectangular_ChecksIsRectangularCorrectly(double a, double b, double c)
+        {
+            var triangle = new Triangle(a, b, c);
+
+            triangle.IsRectangular.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Triangle_WithValidSides_CalculatesAreaCorrectly()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var expectedArea = 6; 
+
+            triangle.Area.Should().BeApproximately(expectedArea, 0.01);
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2)]
+        [InlineData(-1, 10, 12)]
+        public void Triangle_WithInvalidSides_ThrowsIncorrectShapeSizeException(double a, double b, double c)
+        {
+            Action act = () => new TriangleCreator(a, b, c).Create();
+
+            act.Should().Throw<IncorrectShapeSizeException>()
+                .WithMessage($"Triangle with such sides is not valid: {a}, {b}, {c}");
+        }
+    }
+}
